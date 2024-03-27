53d67d9725de8b0c3f9ecf4f940490bedc25249b
Gia Phát <134149572+giaphat1712@users.noreply.github.com>
2024-03-27 13:22:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menubarBtn = document.querySelector('.header-top i')
+menubarBtn.addEventListener("click", function(){
+    document.querySelector('.header-top ul').classList.toggle('active')
+})
+
+
+
+
